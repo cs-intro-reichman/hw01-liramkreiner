9f46eda24e9efa6466b4a915e26d753b0e83c099
liramkreiner <154261070+liramkreiner@users.noreply.github.com>
2023-12-18 19:48:05 +0200

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -3,8 +3,22 @@
  * This is known as the Triangle Inequality Theorem. 
  * Write a program that tests if three given integers form a triangle.
  */ 
-public class Triangle {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+public class Triangle
+{
+    public static void main(String[] args)
+     {
+        //train represent sides of the triangle
+        int train1 = Integer.parseInt(args[0]);
+        int train2 = Integer.parseInt(args[1]);
+        int train3 = Integer.parseInt(args[2]);
+        boolean iftraingle = //checking if any side is grater than the sum of the other two
+        (
+            (train1 + train2) > train3 &
+            (train2 + train3) >  train1 &
+            (train1 + train3) > train2
+        );
+        //printing the sides of the traingle and if it fitts to the form
+        System.out.println( train1 +", " + train2 + ", "+ train3 + ": " + iftraingle);
+    }
 }
+
