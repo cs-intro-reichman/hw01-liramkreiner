8d014cde8ee654fbe1d6ff1ba6f55d25f3e9a788
liramkreiner <154261070+liramkreiner@users.noreply.github.com>
2023-12-18 19:45:22 +0200

    Update AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -1,8 +1,14 @@
 /*
  * Adds two given integers and prints the result in a fancy way.
  */
-public class AddTwo {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+public class AddTwo
+{
+    public static void main(String[] args)
+     {
+        //sum the input getting from the user and than print them
+        int result = Integer.parseInt(args[0]) + Integer.parseInt(args[1]);
+        System.out.println(args[0] + " + " + args[1] + " = " + result);//printing the equation with the result
+    }
 }
+
+	
