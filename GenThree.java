9254a972382606d62c88aef03c691410d1e552e4
liram kreiner <liram.kreiner@post.runi.ac.il>
2023-12-20 09:50:13 +0200

    change name

--- GenThree.java
+++ GenThree.java
@@ -2,7 +2,7 @@
  * Generates three random integers, each in a given range [a,b),
  * prints them, and then prints the minimal number that was generated.
  */
-public class Gen3
+public class GenThree
 {
     public static void main(String[] args)
      {
